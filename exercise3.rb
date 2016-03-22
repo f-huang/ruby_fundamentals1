7173a6a6fb9f2d6db7f009a7f7567f5ff941cee8
Fiona <fionarofl@gmail.com>
2016-03-22 15:03:20 -0400

    Exercise 3 completed

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,12 @@
+# Step 1
 puts "What is your name?"
+name = gets.chomp
+
+puts "Hi #{name}!"
+
+# Step 2
+puts "What year were you born in?"
+year = gets.chomp
+year.to_i
+
+puts "You were born in #{year}."
