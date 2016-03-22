824b4af8bf301c426d666f0b78e07aabe2dae465
Fiona <fionarofl@gmail.com>
2016-03-22 14:34:06 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+## Question 1
+# Define variables for cost of the meal and tip
+meal = 55
+tip_percent = 20
+
+# Calculate tip
+tip_amount = meal * (tip_percent.to_f/100)
+
+# Display tip_amount output in a string
+puts "A good tip for this meal is $#{tip_amount}"
+
+## Quesstion 2
+puts "String" + 2.to_s
+
+## Question 3
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+## Question 4
+puts (10 < 20 && 30 < 20) || !(10 ==1)
